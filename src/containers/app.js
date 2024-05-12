d9e917a4ffcf69845a7c47e303be7d78e9023a30
Ahsan Raza <hp R Y Z E N 5@AhsanRaza.(none)>
2024-05-12 23:56:29 +0500

    hooks added

--- src/containers/app.js
+++ src/containers/app.js
@@ -1,4 +1,4 @@
- import React,{Component} from "react";
+import React,{useState,useEffect} from "react";
 import Cardlist from "../components/Cardlist";
 import Searchbox from "../components/Searchbox";
 import Scroll from '../components/scroll';
@@ -10,58 +10,64 @@ import './app.css';
 
 
 
+function App() {
 
+    // constructor(){
+    //     super()
+    //     this.state={
 
+    //         robots:[],
+    //         searchfield:''
+    //     }
+    // }
+    
+const [robots,setrobots] = useState([])
+const [searchfield, setsearchfield] = useState('')
 
-class app extends Component{
+    // componentDidMount(){
+    //     fetch('https://jsonplaceholder.typicode.com/users ').then(response=>{
+    //         return response.json();
+    //     })
+    // .then(users=> this.setState({robots:users}));
 
-    constructor(){
-        super()
-        this.state={
-
-            robots:[],
-            searchfield:''
-        }
-    }
-    componentDidMount(){
+    // }
+ 
+    //hooks here
+useEffect(()=>{
+    
         fetch('https://jsonplaceholder.typicode.com/users ').then(response=>{
             return response.json();
         })
-    .then(users=> this.setState({robots:users}));
+    .then(users=> {setrobots(users)});
 
+},[])
+
+      const  onsearchchange =(event) =>{
+       setsearchfield(event.target.value)
     }
 
-
-    onsearchchange =(event) =>{
-        this.setState({searchfield: event.target.value})
-        
-  
-
-    }
-    render(){
-
-        const filteredrobots= this.state.robots.filter(robot=>{
-            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        const filteredrobots= robots.filter(robot=>{
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
         })
 
-        if(this.state.robots.length==0){
+        if(robots.length==0){
             return <h1>Loading</h1>
         }
         else{
         return(
             <div className="tc">
                 <h1> ROBOTS</h1>
-                <Searchbox  searchchange={this.onsearchchange}/>
+                <Searchbox  searchchange={onsearchchange}/>
      <Scroll>   
         <ErrorBoundary><Cardlist robots={filteredrobots}/>    
         </ErrorBoundary>     </Scroll>           
               </div>
             );
         }
-    }
+    
 
 
 
 
 }
-export default app;   
+export default App;   
